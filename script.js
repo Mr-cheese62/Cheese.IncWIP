2f1c2e57d41221c1ab439b4a039248b26df1c359
Riley Paffrath <mr.cheese679@gmail.com>
2022-10-19 23:58:54 -0700

    test commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const virusS = document.getElementById('virus')
+virusS.addEventListener('click', startAlert)
+
+function startAlert() {
+    var alert = prompt("hello your computer has virus")
+
+}
